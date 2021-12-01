a83b1c3a355c1b078dd8c236b5528b95196d8333
yourwelcomematt <mattding8@gmail.com>
2021-12-02 09:58:58 +1300

    Created printBoard() method and row properties

--- tictactoe-code/Board.cs
+++ tictactoe-code/Board.cs
@@ -1,7 +1,21 @@
+using System.Text;
+
 namespace kata_tictactoe
 {
     public class Board
     {
-        
+        private string row1 = ". . .";
+        private string row2 = ". . .";
+        private string row3 = ". . .";
+
+        public string printBoard()
+        {
+            var board = new StringBuilder();
+            board.AppendLine(row1);
+            board.AppendLine(row2);
+            board.AppendLine(row3);
+            
+            return board.ToString();
+        }
     }
 }
--- tictactoe-code/Program.cs
+++ tictactoe-code/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!"); 
+using System;
+using kata_tictactoe;
+
+var board = new Board();
+
+Console.WriteLine("Welcome to Tic Tac Toe!");
+Console.WriteLine();
+Console.WriteLine("Here's the current board:");
+Console.WriteLine();
+Console.WriteLine(board.printBoard());
