72dcc4d86e5e432fd1c756dad1a7fe18fa72cb3a
yourwelcomematt <mattding8@gmail.com>
2021-12-02 10:38:43 +1300

    UpdateBoard() method now updates the board using the inputted coordinates and player

--- tictactoe-code/Board.cs
+++ tictactoe-code/Board.cs
@@ -25,9 +25,65 @@ namespace kata_tictactoe
             row3 = ". . .";
         }
 
-        public void UpdateBoard(string coordinates)
+        public void UpdateBoard(string coordinates, string player)
         {
-            
+            switch (coordinates)
+            {
+                case "1, 1" when player.Equals("Player 1"):
+                    row1 = "X . .";
+                    break;
+                case "1, 1" when player.Equals("Player 2"):
+                    row1 = "O . .";
+                    break;
+                case "1, 2" when player.Equals("Player 1"):
+                    row1 = ". X .";
+                    break;
+                case "1, 2" when player.Equals("Player 2"):
+                    row1 = ". O .";
+                    break;
+                case "1, 3" when player.Equals("Player 1"):
+                    row1 = ". . X";
+                    break;
+                case "1, 3" when player.Equals("Player 2"):
+                    row1 = ". . O";
+                    break;
+                case "2, 1" when player.Equals("Player 1"):
+                    row2 = "X . .";
+                    break;
+                case "2, 1" when player.Equals("Player 2"):
+                    row2 = "O . .";
+                    break;
+                case "2, 2" when player.Equals("Player 1"):
+                    row2 = ". X .";
+                    break;
+                case "2, 2" when player.Equals("Player 2"):
+                    row2 = ". O .";
+                    break;
+                case "2, 3" when player.Equals("Player 1"):
+                    row2 = ". . X";
+                    break;
+                case "2, 3" when player.Equals("Player 2"):
+                    row2 = ". . O";
+                    break;
+                case "3, 1" when player.Equals("Player 1"):
+                    row3 = "X . .";
+                    break;
+                case "3, 1" when player.Equals("Player 2"):
+                    row3 = "O . .";
+                    break;
+                case "3, 2" when player.Equals("Player 1"):
+                    row3 = ". X .";
+                    break;
+                case "3, 2" when player.Equals("Player 2"):
+                    row3 = ". O .";
+                    break;
+                case "3, 3" when player.Equals("Player 1"):
+                    row3 = ". . X";
+                    break;
+                case "3, 3" when player.Equals("Player 2"):
+                    row3 = ". . O";
+                    break;
+            }
         }
     }
 }
