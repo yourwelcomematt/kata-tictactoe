24708edb4984d028fc3a1209fd924a35050f3422
yourwelcomematt <mattding8@gmail.com>
2021-12-02 14:39:25 +1300

    Changed UpdateBoard to take symbol instead of player as a parameter, and modified switch-case block to use the symbol instead of explicitly differentiating between players

--- tictactoe-code/Board.cs
+++ tictactoe-code/Board.cs
@@ -16,6 +16,7 @@ namespace kata_tictactoe
             row3 = new List<string>() {".", ".", "."};
         }
         
+        // To test
         public string GenerateBoard()
         {
             var board = new StringBuilder();
@@ -25,66 +26,40 @@ namespace kata_tictactoe
             
             return board.ToString();
         }
-
-        // public void UpdateBoard(string coordinates, string player)
-        // {
-        //     switch (coordinates)
-        //     {
-        //         case "1, 1" when player.Equals("Player 1"):
-        //             row1 = "X . .";
-        //             break;
-        //         case "1, 1" when player.Equals("Player 2"):
-        //             row1 = "O . .";
-        //             break;
-        //         case "1, 2" when player.Equals("Player 1"):
-        //             row1 = ". X .";
-        //             break;
-        //         case "1, 2" when player.Equals("Player 2"):
-        //             row1 = ". O .";
-        //             break;
-        //         case "1, 3" when player.Equals("Player 1"):
-        //             row1 = ". . X";
-        //             break;
-        //         case "1, 3" when player.Equals("Player 2"):
-        //             row1 = ". . O";
-        //             break;
-        //         case "2, 1" when player.Equals("Player 1"):
-        //             row2 = "X . .";
-        //             break;
-        //         case "2, 1" when player.Equals("Player 2"):
-        //             row2 = "O . .";
-        //             break;
-        //         case "2, 2" when player.Equals("Player 1"):
-        //             row2 = ". X .";
-        //             break;
-        //         case "2, 2" when player.Equals("Player 2"):
-        //             row2 = ". O .";
-        //             break;
-        //         case "2, 3" when player.Equals("Player 1"):
-        //             row2 = ". . X";
-        //             break;
-        //         case "2, 3" when player.Equals("Player 2"):
-        //             row2 = ". . O";
-        //             break;
-        //         case "3, 1" when player.Equals("Player 1"):
-        //             row3 = "X . .";
-        //             break;
-        //         case "3, 1" when player.Equals("Player 2"):
-        //             row3 = "O . .";
-        //             break;
-        //         case "3, 2" when player.Equals("Player 1"):
-        //             row3 = ". X .";
-        //             break;
-        //         case "3, 2" when player.Equals("Player 2"):
-        //             row3 = ". O .";
-        //             break;
-        //         case "3, 3" when player.Equals("Player 1"):
-        //             row3 = ". . X";
-        //             break;
-        //         case "3, 3" when player.Equals("Player 2"):
-        //             row3 = ". . O";
-        //             break;
-        //     }
-        // }
+        
+        // To test
+        public void UpdateBoard(string coordinates, string symbol)
+        {
+            switch (coordinates)
+            {
+                case "1, 1":
+                    row1[0] = symbol;
+                    break;
+                case "1, 2":
+                    row1[1] = symbol;
+                    break;
+                case "1, 3":
+                    row1[2] = symbol;
+                    break;
+                case "2, 1":
+                    row2[0] = symbol;
+                    break;
+                case "2, 2":
+                    row2[1] = symbol;
+                    break;
+                case "2, 3":
+                    row2[2] = symbol;
+                    break;
+                case "3, 1":
+                    row3[0] = symbol;
+                    break;
+                case "3, 2":
+                    row3[1] = symbol;
+                    break;
+                case "3, 3":
+                    row3[2] = symbol;
+                    break;
+            }
+        }
     }
 }
