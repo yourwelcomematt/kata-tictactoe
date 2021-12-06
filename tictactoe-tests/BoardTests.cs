bf8b05a438ec61051b96eb6d965bddd8256147ba
yourwelcomematt <mattding8@gmail.com>
2021-12-07 10:44:12 +1300

    Added tests in BoardTests to test initial board generation and board updating after a move is made

--- tictactoe-tests/BoardTests.cs
+++ tictactoe-tests/BoardTests.cs
@@ -1,12 +1,69 @@
+using kata_tictactoe;
 using Xunit;
 
 namespace tictactoe_tests
 {
-    public class UnitTest1
+    public class BoardTests
     {
         [Fact]
-        public void Test1()
+        public void CanGenerate_Board_AfterInitialising()
         {
+            // Arrange
+            Board board = new();
+            // Act
+            board.InitialiseBoard();
+            string actual = board.GenerateBoard();
+            string expected = ". . .\n" + ". . .\n" + ". . .\n";
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [InlineData("X")]
+        [InlineData("O")]
+        [Theory]
+        public void CanUpdate_Row1_AfterMoveIsMade(string symbol)
+        {
+            // Arrange
+            Board board = new();
+            board.InitialiseBoard();
+            // Act
+            board.UpdateBoard("1, 1", symbol);
+            string actual = board.GenerateBoard();
+            string expected = $"{symbol} . .\n" + ". . .\n" + ". . .\n";
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [InlineData("X")]
+        [InlineData("O")]
+        [Theory]
+        public void CanUpdate_Row2_AfterMoveIsMade(string symbol)
+        {
+            // Arrange
+            Board board = new();
+            board.InitialiseBoard();
+            // Act
+            board.UpdateBoard("2, 1", symbol);
+            string actual = board.GenerateBoard();
+            string expected = ". . .\n" + $"{symbol} . .\n" + ". . .\n";
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [InlineData("X")]
+        [InlineData("O")]
+        [Theory]
+        public void CanUpdate_Row3_AfterMoveIsMade(string symbol)
+        {
+            // Arrange
+            Board board = new();
+            board.InitialiseBoard();
+            // Act
+            board.UpdateBoard("3, 1", symbol);
+            string actual = board.GenerateBoard();
+            string expected = ". . .\n" + ". . .\n" + $"{symbol} . .\n";
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
