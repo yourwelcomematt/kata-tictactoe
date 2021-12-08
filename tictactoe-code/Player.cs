fec38d24a8d11ea9026ff8f5a3d58394cfcbd85e
yourwelcomematt <mattding8@gmail.com>
2021-12-09 09:32:00 +1300

    Added tests for assigning and retrieving symbols in PlayerTests, and created Player class along with its constructor and the Symbol property

---
+++ tictactoe-code/Player.cs
@@ -0,0 +1,12 @@
+namespace kata_tictactoe
+{
+    public class Player
+    {      
+        public string Symbol { get; }
+
+        public Player(string symbol)
+        {
+            Symbol = symbol;
+        }
+    }
+}
--- tictactoe-tests/PlayerTests.cs
+++ tictactoe-tests/PlayerTests.cs
@@ -1,7 +1,32 @@
+using kata_tictactoe;
+using Xunit;
+
 namespace tictactoe_tests
 {
     public class PlayerTests
     {
+        [Fact]
+        public void CanRetrieve_XSymbol_FromPlayer()
+        {
+            // Arrange
+            Player player = new("X");
+            // Act
+            string expected = "X";
+            string actual = player.Symbol;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
         
+        [Fact]
+        public void CanRetrieve_OSymbol_FromPlayer()
+        {
+            // Arrange
+            Player player = new("O");
+            // Act
+            string expected = "O";
+            string actual = player.Symbol;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
