7ff583b31aa9e70befb1f47cc194789dcd6becdd
yourwelcomematt <mattding8@gmail.com>
2021-12-13 20:39:01 +1300

    Implemented CheckIfThereAreMovesLeft() in the main game logic, allowing the game to end in a draw when there are no more moves available

--- tictactoe-code/TicTacToeApp.cs
+++ tictactoe-code/TicTacToeApp.cs
@@ -7,17 +7,29 @@ Player player1 = new("Player 1", "X");
 Player player2 = new("Player 2", "O");
 Board board = new();
 GameIo gameIo = new();
+Endgame endgame = new();
 bool gameRunning = true;
+bool movesLeft = true;
 
 Console.WriteLine(gameIo.GetWelcomeMessageString());
 
 while (gameRunning)
 {
     // Player 1's turn
+    movesLeft = endgame.CheckIfThereAreMovesLeft(board.Row1, board.Row2, board.Row3);
+
+    if (!movesLeft)
+    {
+        Console.WriteLine("No more moves available. You have drawn the game");
+        break;
+    }
+    
     string userInput = gameIo.GetUserInput(player1, board).Trim();
 
     if (gameIo.CheckIfQuitting(userInput))
     {
+        Console.WriteLine();
+        Console.WriteLine("Quitting the game...");
         break;
     }
 
@@ -25,11 +37,22 @@ while (gameRunning)
     Console.WriteLine(gameIo.GetMoveAcceptedString());
     Console.WriteLine(board.GetStringOfBoard());
     
+    
     // Player 2's turn
+    movesLeft = endgame.CheckIfThereAreMovesLeft(board.Row1, board.Row2, board.Row3);
+    
+    if (!movesLeft)
+    {
+        Console.WriteLine("No more moves available. The result is a draw.");
+        break;
+    }
+    
     userInput = gameIo.GetUserInput(player2, board).Trim();
     
     if (gameIo.CheckIfQuitting(userInput))
     {
+        Console.WriteLine();
+        Console.WriteLine("Quitting the game...");
         break;
     }
 
