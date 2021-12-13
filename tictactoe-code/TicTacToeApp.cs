990913416b70e61d6f8f4593df91b0122c83877e
yourwelcomematt <mattding8@gmail.com>
2021-12-13 15:35:07 +1300

    Created basic gameplay loop in TicTacToeApp using gameRunning boolean variable

--- tictactoe-code/TicTacToeApp.cs
+++ tictactoe-code/TicTacToeApp.cs
@@ -7,14 +7,19 @@ Player player1 = new("Player 1", "X");
 Player player2 = new("Player 2", "O");
 Board board = new();
 GameDialogue gameDialogue = new();
+bool gameRunning = true;
 
 Console.WriteLine(gameDialogue.GetWelcomeMessageString());
-string userInput = gameDialogue.GetUserInput(player1).Trim();
 
-board.UpdateBoard(userInput, player1.Symbol);
-Console.WriteLine(board.GetStringOfBoard());
+while (gameRunning)
+{
+    string userInput = gameDialogue.GetUserInput(player1).Trim();
 
-userInput = gameDialogue.GetUserInput(player2).Trim();
+    board.UpdateBoard(userInput, player1.Symbol);
+    Console.WriteLine(board.GetStringOfBoard());
 
-board.UpdateBoard(userInput, player2.Symbol);
-Console.WriteLine(board.GetStringOfBoard());
+    userInput = gameDialogue.GetUserInput(player2).Trim();
+
+    board.UpdateBoard(userInput, player2.Symbol);
+    Console.WriteLine(board.GetStringOfBoard());
+}
