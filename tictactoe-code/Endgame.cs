5170b5e495fa11aad0860eabaf8a16ce3d9facc3
yourwelcomematt <mattding8@gmail.com>
2021-12-13 20:26:48 +1300

    Created CheckIfThereAreMovesLeft() and associated tests to return true if there are still moves left and false if there are not

--- tictactoe-code/Endgame.cs
+++ tictactoe-code/Endgame.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+
 namespace kata_tictactoe
 {
     public class Endgame
     {
-        
+        public bool CheckIfThereAreMovesLeft(List<string> row1, List<string> row2, List<string> row3)
+        {
+            return row1[0].Equals(".") || row1[1].Equals(".") || row1[2].Equals(".") || row2[0].Equals(".") ||
+                   row2[1].Equals(".") || row2[2].Equals(".") || row3[0].Equals(".") || row3[1].Equals(".") ||
+                   row3[2].Equals(".");
+        }
     }
 }
--- tictactoe-tests/EndgameTests.cs
+++ tictactoe-tests/EndgameTests.cs
@@ -1,7 +1,60 @@
+using System.Collections.Generic;
+using System.Text;
+using kata_tictactoe;
+using Xunit;
+
 namespace tictactoe_tests
 {
     public class EndgameTests
     {
+        [Fact]
+        public void CheckIfThereAreMovesLeft_ReturnsFalse_IfThereAreNoMovesLeft()
+        {
+            // Arrange
+            Endgame endgame = new();
+            Board board = new();
+            board.Row1 = new List<string>() {"X", "O", "X"};
+            board.Row2 = new List<string>() {"X", "O", "O"};
+            board.Row3 = new List<string>() {"O", "X", "X"};
+            // Act
+            bool expected = false;
+            bool actual = endgame.CheckIfThereAreMovesLeft(board.Row1, board.Row2, board.Row3);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
         
+        [Fact]
+        public void CheckIfThereAreMovesLeft_ReturnsTrue_AtTheStartOfTheGame()
+        {
+            // Arrange
+            Endgame endgame = new();
+            Board board = new();
+            board.Row1 = new List<string>() {".", ".", "."};
+            board.Row2 = new List<string>() {".", ".", "."};
+            board.Row3 = new List<string>() {".", ".", "."};
+            // Act
+            bool expected = true;
+            bool actual = endgame.CheckIfThereAreMovesLeft(board.Row1, board.Row2, board.Row3);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void CheckIfThereAreMovesLeft_ReturnsTrue_IfThereAreMovesLeft()
+        {
+            // Arrange
+            Endgame endgame = new();
+            Board board = new();
+            board.Row1 = new List<string>() {"X", ".", "."};
+            board.Row2 = new List<string>() {".", "X", "."};
+            board.Row3 = new List<string>() {".", "O", "O"};
+            // Act
+            bool expected = true;
+            bool actual = endgame.CheckIfThereAreMovesLeft(board.Row1, board.Row2, board.Row3);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+
     }
 }
